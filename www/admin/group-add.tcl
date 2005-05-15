a1d4ada9d4d01f72651a2c8bcbafe751ce411104
Nick Carroll <nick.c@rroll.net>
2005-05-15 02:36:43 +0000

    Now adds adviser to group when adviser group is created

--- www/admin/group-add.tcl
+++ www/admin/group-add.tcl
@@ -12,11 +12,16 @@ ad_page_contract {
     adviser_id:integer,notnull
 }
 
-# Create group with given group_name.
-set group_id [group::new -group_name $group_name group]
+db_transaction {
+    # Create group with given group_name.
+    set group_id [group::new -group_name $group_name group]
 
-# Use group_id from above and insert with adviser_id into
-# dotfolio_group_adviser_map.
-db_dml insert_group_adviser_ids {}
+    # Add adviser to group.
+    group::add_member -group_id $group_id -user_id $adviser_id
+
+    # Use group_id from above and insert with adviser_id into
+    # dotfolio_group_adviser_map.
+    db_dml insert_group_adviser_ids {}
+}
 
 ad_returnredirect "groups"
